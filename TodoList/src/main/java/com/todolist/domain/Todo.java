72c12a865e9a592ebe33e1b0f88dcdca1153a24b
yerim <na90303@naver.com>
2026-04-07 00:11:09 +0900

    feat: todo domain 생성

---
+++ TodoList/src/main/java/com/todolist/domain/Todo.java
@@ -0,0 +1,58 @@
+package com.todolist.domain;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class Todo {
+    private final Map<Integer, String> TODO_STATUS = Map.of(1, "시작 전", 2, "진행중", 3, "완료");
+
+    private final long id;
+    private String status;
+    private int priority;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Todo(long id, int priority, String content) {
+        this.id = id;
+        this.status = TODO_STATUS.get(1);
+        this.priority = priority;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = LocalDateTime.now();
+    }
+}
