7ee4ec5a2957fd692a9c0642fe660eabdaacf6f9
yerim <na90303@naver.com>
2026-04-07 01:22:44 +0900

    refactor: todoRepository save 함수의 return type 변경

--- TodoList/src/main/java/com/todolist/repository/TodoRepository.java
+++ TodoList/src/main/java/com/todolist/repository/TodoRepository.java
@@ -7,9 +7,8 @@ import java.util.List;
 public class TodoRepository {
     private final List<Todo> todoList = new ArrayList<>();
 
-    public Todo save(Todo todo) {
+    public void save(Todo todo) {
         todoList.add(todo);
-        return todo;
     }
 
     public void findAll() {
